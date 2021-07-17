37a112fb0db2955b128d6a8f2741fa1ed3726662
kuomolew <kuomolew@gmail.com>
2021-07-17 14:46:22 +0300

    start develop

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <AddCard />
 </template>
+
+<script>
+import AddCard from "./components/AddCard.vue";
+
+export default {
+  name: "App",
+  components: {
+    AddCard,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
 
 <style>
 #app {
@@ -13,18 +23,8 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import "~bootstrap/dist/css/bootstrap.css";
 </style>
---
+++ src/components/AddCard.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="container">
+    <button
+      v-if="buttonMode"
+      @click="toggleView"
+      type="button"
+      class="btn btn-primary"
+    >
+      Добавить карту
+    </button>
+
+    <div class="row">
+      <div class="col">
+        <h4>Номер карты</h4>
+        <input type="text" placeholder="Карта" />
+      </div>
+      <div class="col">
+        <h4>Номер телефона</h4>
+        <input type="text" placeholder="Телефон" />
+      </div>
+      <div class="col">
+        <h4>Имя владельца</h4>
+        <input type="text" placeholder="Имя" />
+      </div>
+    </div>
+    <br />
+    <div class="row">
+      <div class="col">
+        <button ttype="button" class="btn btn-success">Добавить карту</button>
+      </div>
+      <div class="col">
+        <button type="button" class="btn btn-secondary">Очистить данные</button>
+      </div>
+      <div class="col">
+        <button type="button" class="btn btn-danger" @click="toggleView">
+          Отменить добавление
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddCard",
+  props: {},
+  data() {
+    return {
+      buttonMode: true,
+    };
+  },
+  methods: {
+    toggleView() {
+      this.buttonMode = !this.buttonMode;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
